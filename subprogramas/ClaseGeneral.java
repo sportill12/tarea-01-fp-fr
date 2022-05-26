471fef753a3f61966444e00e239d8ca4324d3f0f
sportill12 <franckruizfloresmiranda@gmail.com>
2022-05-26 13:55:06 +0000

    gbfodsygfs

---
+++ subprogramas/ClaseGeneral.java
@@ -0,0 +1,21 @@
+package subprogramas;
+
+import java.util.Scanner;
+
+/**
+ * ClaseGeneral
+ */
+public class ClaseGeneral {
+
+    static Scanner lt=new Scanner(System.in);
+    static aritmetica obj=new aritmetica();
+
+    public static void main(String[] args) {
+        System.out.println("Ingrese el Primer Valor:");
+        int aX=lt.nextInt();
+        System.out.println("Ingrese el Segundo Valor:");
+        int bX=lt.nextInt();
+        int result=obj.sumar(aX, bX);
+        System.out.println("La Suma es:"+result);
+    }
+}
---
+++ subprogramas/aritmetica.java
@@ -0,0 +1,15 @@
+package subprogramas;
+
+/**
+ * aritmetica
+ */
+public class aritmetica {
+
+    public int sumar(int a, int b) {
+        return a+b;
+    }
+
+    public int resta(int a, int b) {
+        return a+b;
+    } 
+}
